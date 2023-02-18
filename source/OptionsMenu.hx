66bb1f806968904c5494b0b9391597849f552f03
Random people <98900217+justaguy6@users.noreply.github.com>
2023-02-18 14:04:56 -0300

    Update OptionsMenu.hx

--- source/OptionsMenu.hx
+++ source/OptionsMenu.hx
@@ -34,6 +34,7 @@ class OptionsMenu extends MusicBeatState
 
 	var options:Array<OptionCategory> = [
 		new OptionCategory("Sonic exe", [
+			new AndroidControls(),
 			new JumpscareOption("Displays jumpscares in some songs (this affects the gameplay preformance by alot)"),
 			new Vfx("Enables special visual effects (turning it off helps with memory and preformace)"),
 			new SplashOption("Enables splattering blood on SICK! hits."),
@@ -46,9 +47,9 @@ class OptionsMenu extends MusicBeatState
 			new MiddlescrollOption("Sets the strumline to the middle of the screen and hides the opponent's."),
 			new GhostTapOption("Ghost Tapping is when you tap a direction and it doesn't give you a miss."),
 			new Judgement("Customize your Hit Timings (LEFT or RIGHT)"),
-			#if desktop
+			
 			new FPSCapOption("Cap your FPS"),
-			#end
+			
 			new ScrollSpeedOption("Change your scroll speed (1 = Chart dependent)"),
 			new AccuracyDOption("Change how accuracy is calculated. (Accurate = Simple, Complex = Milisecond Based)"),
 			new ResetButtonOption("Toggle pressing R to gameover."),
@@ -58,28 +59,27 @@ class OptionsMenu extends MusicBeatState
 		new OptionCategory("Appearance", [
 			new DistractionsAndEffectsOption("Toggle stage distractions that can hinder your gameplay."),
 			new CamZoomOption("Toggle the camera zoom in-game."),
-			#if desktop
+			
 			new RainbowFPSOption("Make the FPS Counter Rainbow"),
 			new AccuracyOption("Display accuracy information."),
 			new NPSDisplayOption("Shows your current Notes Per Second."),
 			new SongPositionOption("Show the songs current position (as a bar)"),
 			new CpuStrums("CPU's strumline lights up when a note hits it."),
-			#end
+			
 		]),
 		
 		new OptionCategory("Misc", [
-			#if desktop
+			
 			new FPSOption("Toggle the FPS Counter"),
-			new ReplayOption("View replays"),
-			#end
+			
 			new FlashingLightsOption("Toggle flashing lights that can cause epileptic seizures and strain."),
 			new WatermarkOption("Enable and disable all watermarks from the engine."),
 			new ShowInput("Display every single input in the score screen."),
 			//new Optimization("No backgrounds, no characters, centered notes, no player 2.")
 			new BotPlay("Showcase your charts and mods with autoplay."),
-			#if
+			
 			new RealBot('The coolest botplay')
-			#end
+			
 		])
 		
 	];
@@ -131,6 +131,10 @@ class OptionsMenu extends MusicBeatState
 		FlxTween.tween(versionShit,{y: FlxG.height - 18},2,{ease: FlxEase.elasticInOut});
 		FlxTween.tween(blackBorder,{y: FlxG.height - 18},2, {ease: FlxEase.elasticInOut});
 
+		#if android
+		addVirtualPad(FULL, A_B);
+		#end
+			
 		super.create();
 	}
 
@@ -183,9 +187,9 @@ class OptionsMenu extends MusicBeatState
 					}
 				}
 				
-				if (FlxG.keys.justPressed.UP)
+				if (controls.UP_P)
 					changeSelection(-1);
-				if (FlxG.keys.justPressed.DOWN)
+				if (controls.DOWN_P)
 					changeSelection(1);
 			}
 			
